bfc1de92bc8fbe33af4e96a99e27c7f53b2e78d1
Tom I <tom.ille@gmx.de>
2019-08-18 14:29:53 +0200

    collision projection via gmtl

--- PlasticDeformationDLL/LinearAlgebra.cpp
+++
@@ -1,8 +0,0 @@
-#include "LinearAlgebra.h"
-
-LinearAlgebra::LinearAlgebra(){}
-LinearAlgebra::~LinearAlgebra(){}
-
-float LinearAlgebra::tAtLinePlaneIntersection(Vector3f planePoint, Vector3f planeNormal, Vector3f lineOrigin, Vector3f lineDirection){
-	return (-planeNormal).dot(lineOrigin - planePoint) / planeNormal.dot(lineDirection - lineOrigin);
-}
